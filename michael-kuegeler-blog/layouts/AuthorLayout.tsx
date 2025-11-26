50b6c0b7d4bbc709c34322fe170fc16174500794
Michael Kuegeler <mkuegeler@mac.com>
2025-11-26 17:58:46 +0000

    Start page updated.

--- michael-kuegeler-blog/layouts/AuthorLayout.tsx
+++ michael-kuegeler-blog/layouts/AuthorLayout.tsx
@@ -22,31 +22,8 @@ export default function AuthorLayout({ children, content }: Props) {
             Computer, Kunst, Geschichte(n).
           </p>
         </div>
-        <div className="items-start space-y-0 space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8">
-          <div className="flex flex-col items-center space-x-2 pt-8">
-            {avatar && (
-              <Image
-                src={avatar}
-                alt="avatar"
-                width={192}
-                height={192}
-                className="h-48 w-48 rounded-full"
-              />
-            )}
-            <h3 className="pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="x" href={twitter} />
-              <SocialIcon kind="bluesky" href={bluesky} />
-            </div>
-          </div>
-          <div className="prose dark:prose-invert max-w-none pt-8 pb-8 xl:col-span-2">
-            {children}
-          </div>
+        <div className="prose dark:prose-invert container max-w-none py-12 pt-8 pb-8 xl:col-span-2">
+          {children}
         </div>
       </div>
     </>
